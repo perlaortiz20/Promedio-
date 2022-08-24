4d4d5c8d589c824a64e7747f348f52495672c8cf
perlaortiz20 <111938045+perlaortiz20@users.noreply.github.com>
2022-08-23 23:42:18 -0500

    Add files via upload

---
+++ Promedio-Perla Ortiz.py
@@ -0,0 +1,21 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print("Ingrese la cantidad de datos")
+	numeroa = float()
+	numerob = float()
+	numeroc = float()
+	prom = float()
+	print("Ingrese numero a")
+	numeroa = float(input())
+	print("Ingrese numero b")
+	numerob = float(input())
+	print("Ingrese numero c")
+	numeroc = float(input())
+	# proceso
+	prom = (numeroa+numerob+numeroc)/3
+	print(" El promedio es:",prom)
+
